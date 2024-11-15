ea950d2473d67f5be2af026eeb3ee8f5f7e6403a
Anastasiia Alekseeva <alekseevaas21@gmail.com>
2024-11-15 12:02:43 +0100

    fix: handle project ID type with await params

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -5,26 +5,27 @@ import { auth } from '@clerk/nextjs/server';
 import { TaskList } from './task-list';
 
 
-export default async function Project({ params}: { params: { id: string } }) {
-  const projectId = parseInt(params.id,10);
-  if (isNaN(projectId)) {
+export default async function Project({ params }: { params: { id: number } }) {
+  const { id } = await params;
+
+  if (isNaN(id)) {
     return <h1>Invalid project ID</h1>;
   }
   
   const { userId } = await auth();
 
-  const projects = await db.select().from(projectTable).where(eq(projectTable.id, projectId));
+  const projects = await db.select().from(projectTable).where(eq(projectTable.id, id));
   const project = projects[0];
 
   if (project.userId !== userId) {
     return <h1>Not allowed to access project</h1>;
   }
 
-  const tasks = await db.select().from(taskTable).where(eq(taskTable.projectId, projectId));
+  const tasks = await db.select().from(taskTable).where(eq(taskTable.projectId, id));
 
   return (
     <div>
-      <TaskList projectId={projectId} tasks={tasks} />;
+      <TaskList projectId={id} tasks={tasks} />;
     </div>
   );
 }
